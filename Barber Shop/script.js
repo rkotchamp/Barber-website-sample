90f06082e4bd90311e7155235f10607a0f8231c4
rkotchamp <106193123+rkotchamp@users.noreply.github.com>
2023-03-01 04:52:15 +0200

    Burger on mobile Screen
    i made some few changes to the burger three bars so that it can open close when clicked. I also made some few changes to the interface .

--- Barber Shop/script.js
+++ Barber Shop/script.js
@@ -1,1 +1,38 @@
 "use strict";
+//selecting elements
+const hidden = document.querySelector(".hidden");
+const burgar = document.querySelector(".burgar-bars");
+const socialsOverlay = document.querySelector(".socials");
+// const socialsHidden = document.querySelector(".socials-hidden");
+const burgerNav = document.querySelector(".burgerNav");
+const timesButton = document.querySelector(".timesButton");
+
+// Manipulating the burgar pop up menu and the closing button
+// what should be done
+socialsOverlay.classList.add("hidden");
+
+// close Button function
+const closeOverlay = function () {
+  socialsOverlay.classList.remove("hidden");
+  timesButton.classList.remove("hidden");
+  // timesButton.classList.remove("hidden");
+};
+
+const openOverlay = function () {
+  socialsOverlay.classList.add("hidden");
+  timesButton.classList.add("hidden");
+};
+
+// creating events
+burgar.addEventListener("click", closeOverlay);
+
+// when you click on  the times button it must close the pop up menu
+timesButton.addEventListener(
+  "click",
+  openOverlay
+  // timesButton.classList.add("hidden");
+  // socialsOverlay.classList.add("hidden");
+);
+
+// Manipulating the navigation menu
+burgerNav.classList.add("hidden");
